0b43bf114879a11c34c7c682dc7cdc9271fa4559
chanmi24 <chanmi24@gmail.com>
2026-05-07 16:00:29 +0900

    fix(schedule):라우트수정

--- src/router/routes/routeSchedule.js
+++ src/router/routes/routeSchedule.js
@@ -1,7 +1,15 @@
 const url = 'calendar'
 
 export const scheduleRoutes = [
-
+  {
+    path: `${url}/schedules`,
+    component: () => import('@/views/academic/auth/Schedule.vue'),
+    meta: {
+      title: '학사 일정 관리',
+      groupTitle: '학사 일정',
+      auth: ['PROFESSOR', 'STUDENT'],
+    },
+  },   
 ]
 
 export const adminScheduleRoutes =[
