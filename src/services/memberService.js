6bd211c756962a948251fb73d72e04c226acccb9
ksy <tndud4615@gmail.com>
2026-05-12 01:50:09 +0900

    fix(member) 내 계정 수정. 코드 목록 기능 분리

--- src/services/memberService.js
+++ src/services/memberService.js
@@ -23,11 +23,6 @@ class MemberService {
 
 
   //////////// 계정 생성 //////////////
-  // 역할 조회
-  async getMemberRole() {
-    const res = await axios.get('/auth/code?code_type=memberRole')
-    return res.data;
-  }
 
   async createStudent(formData) {
     const res = await axios.post(`${this.#adminPath}/students`, formData)
@@ -45,34 +40,6 @@ class MemberService {
   // 학과 목록 조회
   async getMajorList() {
     const res = await axios.get(`${this.#path}/majors`)
-    return res.data;
-  }
-
-  // 학생 상태 목록 조회
-  async getStudentStatusList() {
-    const res = await axios.get('/auth/code?code_type=studentStatus')
-    return res.data;
-  }
-  // 교수 상태 목록 조회
-  async getProfessorStatusList() {
-    const res = await axios.get('/auth/code?code_type=professorStatus')
-    return res.data;
-  }
-  async getProfessorPositionList() {
-    const res = await axios.get('/member/code?code_type=professorPosition')
-    return res.data;
-  }
-  async getProfessorDegreeList() {
-    const res = await axios.get('/member/code?code_type=professorDegree')
-    return res.data;
-  }
-  async getBuildingList() {
-    const res = await axios.get('/auth/code?code_type=building')
-    return res.data;
-  }
-  // 관리자 상태 목록 조회
-  async getAdminStatusList() {
-    const res = await axios.get('/member/code?code_type=adminStatus')
     return res.data;
   }
 
