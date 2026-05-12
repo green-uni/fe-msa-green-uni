50429fdc9fc11ebb9ba474b0eb6187d1cc8d064a
ksy <tndud4615@gmail.com>
2026-05-12 18:33:56 +0900

    feat(auth) 이메일와 회원코드로 인증 기능 작업중

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -1,6 +1,11 @@
 import axios from './httpRequester'
 
 class MailService {
+    #auth = '/auth'
+    #path = '/emails'
+
+    async sendCode(data) { return await axios.post(`${this.#auth}${this.#path}`, data) }
+    async checkCode(data) { return await axios.post(`${this.#auth}${this.#path}/verification`, data) }
 }
 
 export default new MailService();
