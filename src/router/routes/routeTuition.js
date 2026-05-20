b60749a270ade35091a805daea94673cf30931ce
junlee <myyouthhada@naver.com>
2026-05-20 11:22:20 +0900

    [feat] 등록금 학생 부분

--- src/router/routes/routeTuition.js
+++ src/router/routes/routeTuition.js
@@ -1,7 +1,27 @@
-const url = 'tuitions'
+const studentUrl = 'tuitions'
+const adminUrl = 'admin'
 
 export const tuitionRoutes = [
-
+    {
+    path: studentUrl,
+    component: () => import('@/views/admin/major/AdminMajorList.vue'),
+    meta: {
+      title: '등록금 조회',
+      groupTitle: '등록금 관리',
+      auth: ['ADMIN'],
+    },
+  },
+  {
+    path: `${studentUrl}/:majorId/edit`,
+    component: () => import('@/views/admin/major/AdminMajorCreateEdit.vue'),
+    meta: {
+      title: '학과 정보수정',
+      groupTitle: '등록금 관리',
+      showInNav: false,
+      activeMenu: '/admin/majors',
+      auth: ['ADMIN'],
+    },
+  },
 ]
 
 export const adminTuitionRoutes =[
